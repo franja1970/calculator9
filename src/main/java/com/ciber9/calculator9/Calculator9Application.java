b26097626d4881bb1e73a199a95410d27f4ff93e
Javier Pérez-Fontán <jpfontan.cifp@gmail.com>
2022-05-03 09:46:36 +0000

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber9/calculator9/Calculator9Application.java
@@ -0,0 +1,13 @@
+package com.ciber9.calculator9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator9Application.class, args);
+	}
+
+}
